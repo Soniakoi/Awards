f49376470f8e8370974c3df6e1b79df056f980de
Soniawangui <koisonia99@gmail.com>
2019-08-05 01:30:12 +0300

    Import models class,then create the project class that inherits from the modules.Model class,then create a five class variable that represents different columns in my database

--- award/models.py
+++ award/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+  name=models.CharField(max_length=40)
+  image=models.ImageField(upload_to='projects/',default='tech.jpeg')
+  user=models.ForeignKey(User,on_delete=models.CASCADE)
+  description=models.TextField()
+  link=models.CharField(max_length=60,default='site.com')
+
+  def __str__(self):
+    return self.name
+
+  def project_save(self):
+    self.save()
+
+  def project_delete(self):
+    self.delete()
+
+  class Meta:
+    ordering=['name']
+
