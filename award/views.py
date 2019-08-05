91b923073bf5833e7dc3efbabee312bbc2b9aced
Soniawangui <koisonia99@gmail.com>
2019-08-05 12:03:40 +0300

    Create a d view function that holds the profile and also the login

--- award/views.py
+++ award/views.py
@@ -25,5 +25,49 @@ def project(request,project_id):
         project = Project.objects.get(id = project_id)
     except DoesNotExist:
         raise Http404()
-    return render(request,"all-awards/project.html", {"project":project})        
+    return render(request,"all-awards/project.html", {"project":project}) 
+
+
+def profile(request):
+  current_user = request.user
+  profile=Profile.objects.filter(user_id=current_user.id)
+  projects = Project.objects.filter(user_id=current_user.id)
+
+  return render(request,'profile.html',{"profile":profile,"projects":projects})
+
+@login_required(login_url='/accounts/login')
+def new_profile(request):
+  current_user=request.user
+  if request.method == 'POST':
+    form=NewProfileForm(request.POST,request.FILES)
+    if form.is_valid():
+      profile = form.save(commit=False)
+      profile.user = current_user
+      prof_pic=form.cleaned_data['prof_pic']
+      bio=form.cleaned_data['bio']
+      contact = form.cleaned_data['contact']
+      Profile.objects.filter(user=current_user).update(bio=bio,prof_pic=prof_pic,contact=contact)
+      profile.save()
+
+    return redirect('profile') 
+  else:
+    form = NewProfileForm()
+    return render(request,'new-profile.html',{"form":form})
+
+@login_required(login_url='/accounts/login')
+def project_add(request):
+  current_user = request.user
+  if request.method == 'POST':
+    form = NewProjectForm(request.POST,request.FILES)
+    if form.is_valid():
+      project = form.save(commit=False)
+      project.user = current_user
+      project.save()
+    return redirect('home')
+
+  else:
+    form=NewProjectForm()
+    return render(request,'project-add.html',{'form':form})
+
+
 
