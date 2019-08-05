c49e8035a412cd2b6e4a2f7f7dc5031ed0790f0b
Soniawangui <koisonia99@gmail.com>
2019-08-05 13:00:46 +0300

    Remove html files from a folder

--- award/urls.py
+++ award/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url(r'^search/', views.search,name='search'),
-    url(r'^project/(\d+)',views.project,name ='project')
+    url(r'^project/(\d+)',views.project,name ='project'),
     url(r'^new/profile$',views.new_profile,name='newProfile'),
 
 ]
