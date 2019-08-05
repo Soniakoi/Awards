405b6b0cc9419841d75dde7889623078c9a6018d
Soniawangui <koisonia99@gmail.com>
2019-08-05 09:11:01 +0300

    Create a form inside the nav tags in our navbar.html

--- award/admin.py
+++ award/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project,Profile
 
 # Register your models here.
 admin.site.register(Project)
-# admin.site.register(Article)
-# admin.site.register(tags)
+admin.site.register(Profile)
